db8525c2bcab08652253c26a15d793434541c697
misha <user@users-MacBook-Air.local>
2024-05-21 10:22:22 -0400

    first commmit

---
+++ gui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk() # instantiate an instance of a window 
+window.geometry("330x270")
+window.title("M's first GUI Program")
+window.config(background= '#7e8ea8')
+
+window.mainloop() # place window on computer screen, listen for events 
+
